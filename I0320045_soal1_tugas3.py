d730f6757d0e6e8c97ed176e79eefd0d5d42fda6
HarryBrahmana <80480107+HarryBrahmana@users.noreply.github.com>
2021-03-19 17:48:47 +0700

    Add files via upload
    Harry-Brahmana_I0320045_Andhika_Tugas3

---
+++ I0320045_soal1_tugas3.py
@@ -0,0 +1,28 @@
+#Soal nomor 1 Tugas Pertemuan 3
+
+#Mulai
+
+#Daftar nama yang akan dimasukkan : Nauval, Vika, Iffa, Issa, Ojat, Afnan, Jefri, Ivan, Rafli, Ahmed
+
+list : ["Nauval", "Vika", "Iffa", "Issa", "Ojat", "Afnan", "Jefri", "Ivan", "Rafli", "Ahmed"]
+
+#print list 4, 6, 7
+print (list[4])
+print (list[6])
+print (list[7])
+
+#ganti nama yang ada pada index list 3, 5, 9
+list[3]="Azzahra"
+list[6]="Safely"
+list[9]="Ridwan"
+
+#penambahan 2 anggota baru ke index list
+list.append("Narista")
+list.append("Rara")
+print(list)
+
+for i in list :
+    print = ("{}".format(i))
+
+#menampilkan semua daftar nama pada index list dengan perulangan
+print = ("panjang list adalah :", len(list))
---
+++ I0320045_soal2_tugas3.py
@@ -0,0 +1,41 @@
+#Dictionary
+#Mulai
+#Note Dadi = Data Diri
+
+Dadi= {"Nama": "Harry Brahmana",
+       "Hobi1": "Berolahraga",
+       "Hobi2": "Mendengarkan musik",
+       "Hobi3": "Bermain game",
+       "Hobi4": "Menonton film",
+       "Social Media FB": "Harry Brahmana",
+       "Social Media IG": "@harry_brahmana",
+       "Social Media Twitter": "BrahmanaHarry18",
+       "Social Media Tiktok": "@harry_brahmana & withharryb",
+       "Lagu Kesukaan1": "To The Bone - Pamungkas",
+       "Lagu Kesukaan2": "Love is Gone - Slander ft. Dylan Matthew",
+       "Lagu Kesukaan3": "Heartbreak Anniversary - Giveon",
+       "Makanan Favorit1": "Mie",
+       "Makanan Favorit2": "Ayam semur atau kecap",
+       "Makanan Favorit3": "Nasi Goreng"}
+
+print (Dadi)
+
+#mengubah hobi
+Dadi['Hobi3']= 'Membaca Komik'
+print(Dadi)
+
+#mengubah sosial media
+Dadi['Social Media Fb']= 'Harry Brahmana 2'
+print(Dadi)
+
+#menghapus 2 makanan favorit
+del Dadi["Makanan Favorit1"]
+print(Dadi)
+del Dadi["Makanan Favorit2"]
+print(Dadi)
+
+#menambah 1 hobi
+Dadi['Hobi3'] = "Bernyanyi"
+print Dadi
+
+#Selesai
